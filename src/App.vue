c8205352d53773f987014dbdf7c72dcb2030f3e5
gyd1997 <gyd0008@163.com>
2020-11-01 22:32:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <router-view></router-view>
     <div>
-      <router-link to="/home">首页</router-link>
+      <router-link to="/home">首页&nbsp;&nbsp;</router-link>
       <router-link to="/login">登录</router-link>
     </div>
     <img alt="Vue logo" src="./assets/img/logo.png">
-    <HelloWorld msg="a coderwhy vue template"/>
+    <HelloWorld msg="one step to create project for you"/>
   </div>
 </template>
 
